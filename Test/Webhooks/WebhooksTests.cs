b7e1b5f3047b6d863020c402c197d8c09cac5fa8
David Melendez <dave_melendez@hotmail.com>
2023-04-05 18:39:13 -0500

    version and test stubs

---
+++ Test/Webhooks/WebhooksTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Net.Http;
+using System.Collections.Generic;
+using PayPalHttp;
+using Xunit;
+using PayPalCheckoutSdk.Test;
+using static PayPalCheckoutSdk.Test.TestHarness;
+using System.Threading.Tasks;
+
+namespace PayPalCheckoutSdk.Webhooks.Test
+{
+    [Collection("Webhooks")]
+    public class WebhooksTests
+    {
+        [Fact]
+        public async void TestWebhooksGetEmptyRequest()
+        {
+            //TODO: Tests
+        }
+
+        [Fact]
+        public async Task TestCreateGetDeleteWebhooksRequest()
+        {
+            //TODO: Tests
+
+        }
+    }
+}
