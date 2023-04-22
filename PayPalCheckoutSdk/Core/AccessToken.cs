29076b02241ac409931b195a8e5b55359d3bf199
David Melendez <dave_melendez@hotmail.com>
2023-04-22 09:26:04 -0500

    #10 fix AccessToken createdDate bug, v2.0.1

--- PayPalCheckoutSdk/Core/AccessToken.cs
+++ PayPalCheckoutSdk/Core/AccessToken.cs
@@ -6,7 +6,10 @@ namespace PayPalCheckoutSdk.Core
 {
     [DataContract]
     public class AccessToken
-    {
+    {        
+        private DateTime _createDate;
+        private int expiresIn;
+
         [DataMember(Name = "access_token")]
         [JsonPropertyName("access_token")]
         public string? Token { get; set; }
@@ -17,10 +20,24 @@ namespace PayPalCheckoutSdk.Core
 
         [DataMember(Name = "expires_in")]
         [JsonPropertyName("expires_in")]
-        public int ExpiresIn { get; set; }
+        public int ExpiresIn 
+        { 
+            get => expiresIn;
+            set 
+            { 
+                expiresIn = value;
+                //Fixes a bug where an implicit cast does not set the _createdDate
+                if (_createDate == DateTime.MinValue)
+                {
+                    _createDate = DateTime.UtcNow;
+                }
+            }
+        }
 
-        private readonly DateTime _createDate;
-
+        /// <summary>
+        /// This constructor sometimes does not set the _createdDate 
+        /// because the HttpClient does an implicit cast for the result and does not create a new instance of this object
+        /// </summary>
         public AccessToken()
         {
             _createDate = DateTime.UtcNow;
