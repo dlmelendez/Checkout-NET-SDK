17500c5b3b1fb21fd407be36229f5e8f9f213e69
David Melendez <dave_melendez@hotmail.com>
2025-12-17 11:11:39 -0600

    suppress warning

--- PayPalCheckoutSdk/ServiceExtensions.cs
+++ PayPalCheckoutSdk/ServiceExtensions.cs
@@ -3,7 +3,9 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 
+#pragma warning disable IDE0130 // Namespace does not match folder structure
 namespace Microsoft.Extensions.DependencyInjection
+#pragma warning restore IDE0130 // Namespace does not match folder structure
 {
     /// <summary>
     /// Used for startup service dependency injection
